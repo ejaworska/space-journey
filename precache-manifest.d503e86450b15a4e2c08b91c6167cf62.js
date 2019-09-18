38871b1b964e24e96f79235b966fa76b21188bb4
ejaworska <ewel.jaworska.com>
2019-09-18 16:13:45 +0200

    Updates

---
+++ precache-manifest.d503e86450b15a4e2c08b91c6167cf62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e60117c225b27db921a50790f1de5abd",
+    "url": "/space-journey/index.html"
+  },
+  {
+    "revision": "cc44ed43c9a176bc733a",
+    "url": "/space-journey/static/css/2.cc034990.chunk.css"
+  },
+  {
+    "revision": "cc44ed43c9a176bc733a",
+    "url": "/space-journey/static/js/2.68bab9fe.chunk.js"
+  },
+  {
+    "revision": "4b45ccface8505441c5f",
+    "url": "/space-journey/static/js/main.4ead9a62.chunk.js"
+  },
+  {
+    "revision": "048a94f65488fe0d6f03",
+    "url": "/space-journey/static/js/runtime~main.1b11ff66.js"
+  },
+  {
+    "revision": "9155b3c94383caa22f1c333f4cb84431",
+    "url": "/space-journey/static/media/night.9155b3c9.jpg"
+  }
+]);
